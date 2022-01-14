648bc7ccef6e56fbda749f8f15cbcce8f5f5335a
Abel Fernando Sangama Oñate <abelsangama@upeu.edu.pe>
2022-01-14 16:52:53 -0500

    subiendo proyecto

---
+++ android/app/src/main/kotlin/com/ferdidand79/flutter_game_ahorcado/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ferdidand79.flutter_game_ahorcado
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({ Key? key }) : super(key: key);
+
+  @override
+  _HomeAppState createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
